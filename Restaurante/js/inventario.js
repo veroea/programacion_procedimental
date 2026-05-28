cd916497ba83a601687c9b378a21502ecbc62497
veronica escobar <veaaguirre1763@gmail.com>
2026-05-28 17:13:49 -0500

    get_user_information

---
+++ Restaurante/js/inventario.js
@@ -0,0 +1,4 @@
+let codigoProducto = document.getElementById("codigoProducto").value;
+let nombreProducto = document.getElementByName("nombreProducto").value;
+let cantidad = document.getElementById("cantidad").value;
+let marcaProducto = document.getElementByName("marcaProducto").value;
