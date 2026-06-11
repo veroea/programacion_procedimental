a73070e187e8c9be30ddd15583e1f9fbb115ac6a
veronica escobar <veaaguirre1763@gmail.com>
2026-06-11 17:49:47 -0500

    validate_module_orders: validar datos

--- Restaurante/js/pedidos.js
+++ Restaurante/js/pedidos.js
@@ -5,3 +5,30 @@ let direccion = document.getElementById("direccion").value;
 let telefono = document.getElementById("telefono").value;
 let metodoPago = document.getElementByName("metodoPago").value;
 let observaciones = document.getElementByName("observaciones").value;
+
+function ValidarDatos() {
+    if (nombreUsuario == '' || producto == '' || cantidad == '' || direccion == '' || telefono == '') {
+        console.log('Los campos estan vacios');
+    }
+    else {
+        if (nombreUsuario != /[a-zA-Z]/) {
+            alert('El nombre de usuario no acepta números');
+        }
+
+        if (producto != /[a-zA-Z]/) {
+            alert('El producto no acepta números');
+        }
+
+        if (cantidad != /[^\d+$]/) {
+            alert('La cantidad solo acepta números');
+        }
+
+        if (direccion == '') {
+            alert('La dirección no puede estar vacía');
+        }
+
+        if (telefono != /[^\d+{1,10}$]/) {
+            alert('El teléfono solo acepta números y debe tener entre 1 y 10 dígitos');
+        }
+    }
+}
