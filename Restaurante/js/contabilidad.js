8286cd017027f21b219b7be40a07bdf92ed033cd
veronica escobar <veaaguirre1763@gmail.com>
2026-06-11 15:47:16 -0500

    main: validar datos contabilidad}

--- Restaurante/js/contabilidad.js
+++ Restaurante/js/contabilidad.js
@@ -5,3 +5,32 @@ let ingresos = document.getElementById("ingresos").value;
 let egresos = document.getElementById("egresos").value;
 let concepto = document.getElementByName("concepto").value;
 let valor = document.getElementById("valor").value;
+
+function ValidarDatos() {
+    if (nombre == ''|| ventas == '' || ingresos == '' || egresos == '' || valor == '' ){
+        console.log('Los campos estan vacios')
+    }
+    else{
+        if (nombre == ""){
+        alert("El campo está vacío");
+        return false;
+        }
+        if (ventas != /[^\d+$]/){
+            alert('Este campo solo acepta números')
+            return false;
+        }
+        if (ingresos != /[^\d+$]/) {
+            alert('Este campo solo acepta números')
+            return false;
+        }
+        if (egresos != /[^\d+$]/){
+            alert('Este campo solo acepta números')
+            return false;
+        }
+        if (valor != /[^\d+$]/){
+            alert('Este campo solo acepta números')
+            return false;
+        }
+        return true;
+    }
+}
--- Restaurante/js/usuario.js
+++ Restaurante/js/usuario.js
@@ -11,31 +11,36 @@ let fechaNacimiento = document.getElementById("fechaNacimiento").value;
 let contraseña = document.getElementById("contraseña").value;
 
 function ValidarDatos() {
-    // Nombre
-    if (nombre == '' || apellido == '' || numeroDocumento == '' || telefono == '' || email == '' ){
-        console.log("Los campos estan vacios")
+  // Nombre
+  if (
+    nombre == "" ||
+    apellido == "" ||
+    numeroDocumento == "" ||
+    telefono == "" ||
+    email == ""
+  ) {
+    console.log("Los campos estan vacios");
+  } else {
+    if (nombre != /[a-zA-Z]/) {
+      alert("Este campo no acepta números");
+      return false;
     }
-    else{
-        if (nombre != /[a-zA-Z]/){
-            alert('Este campo no acepta números')
-            return false;
-        }
-        if (apellido != /[a-zA-Z]/){
-            alert('Este campo no acepta números')
-            return false;
-        }
-        if (numeroDocumento != /[^\d+$]/){
-            alert('Este campo solo acepta números')
-            return false;
-        }
-        if (telefono != /[^\d{1,10}$]/){
-            alert('Este campo solo acepta números y solo se permiten 10 digitos')
-            return false;
-        }
-        if (correo != include("@")){
-            alert('Este campo debe incluir @')
-            return false;
-        }
+    if (apellido != /[a-zA-Z]/) {
+      alert("Este campo no acepta números");
+      return false;
+    }
+    if (numeroDocumento != /[^\d+$]/) {
+      alert("Este campo solo acepta números");
+      return false;
+    }
+    if (telefono != /[^\d{1,10}$]/) {
+      alert("Este campo solo acepta números y solo se permiten 10 digitos");
+      return false;
+    }
+    if (correo != include("@")) {
+      alert("Este campo debe incluir @");
+      return false;
     }
     return true;
+  }
 }
