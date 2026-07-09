327cd00070978b1886557564d30b70010ed08253
veronica escobar <veaaguirre1763@gmail.com>
2026-07-09 16:38:59 -0500

    validar datos

--- Restaurante/js/usuario.js
+++ Restaurante/js/usuario.js
@@ -1,40 +1,39 @@
-console.log("Hola mundo");
-let nombre = document.getElementByName("nombre").value;
-let apellido = document.getElementByName("apellido").value;
-let tipoDocumento = document.getElementByName("tipoDocumento").value;
+function ValidarDatos() {
+  alert("La función se ejecutó");
+let nombre = document.getElementById("nombre").value;
+let apellido = document.getElementById("apellido").value;
+let tipoDocumento = document.getElementById("tipoDocumento").value;
 let numeroDocumento = document.getElementById("numeroDocumento").value;
 let telefono = document.getElementById("telefono").value;
-let email = document.getElementByName("email").value;
-let genero = document.getElementByName("genero").value;
-let cargo = document.getElementByName("cargo").value;
+let email = document.getElementById("email").value;
+let genero = document.getElementById("genero").value;
+let cargo = document.getElementById("cargo").value;
 let fechaNacimiento = document.getElementById("fechaNacimiento").value;
 let contraseña = document.getElementById("contraseña").value;
 
-function ValidarDatos() {
-  // Nombre
   if (
-    nombre == "" ||
-    apellido == "" ||
-    numeroDocumento == "" ||
-    telefono == "" ||
-    email == ""
+    nombre === "" ||
+    apellido === "" ||
+    numeroDocumento === "" ||
+    telefono === "" ||
+    email === ""
   ) {
     console.log("Los campos estan vacios");
   } else {
-    if (nombre != /[a-zA-Z]/) {
+    if (!/[a-zA-Z]/.test(nombre)) {
+    alert("Este campo no acepta números");
+    }
+    if (! /[a-zA-Z]/.test(apellido)) {
       alert("Este campo no acepta números");
     }
-    if (apellido != /[a-zA-Z]/) {
-      alert("Este campo no acepta números");
+    if (!/^\d+$/.test(numeroDocumento)) {
+    alert("Este campo solo acepta números");
     }
-    if (numeroDocumento != /[^\d+$]/) {
-      alert("Este campo solo acepta números");
+    if (!/^\d{1,10}$/.test(telefono)) {
+    alert("Este campo solo acepta números y debe tener entre 1 y 10 dígitos");
     }
-    if (telefono != /[^\d{1,10}$]/) {
-      alert("Este campo solo acepta números y solo se permiten 10 digitos");
-    }
-    if (correo != include("@")) {
-      alert("Este campo debe incluir @");
+    if (!email.includes("@")) {
+    alert("Este campo debe incluir @");
     }
   }
 }
